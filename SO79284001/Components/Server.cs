efb9bc9ef3c657b86f1dd15110ed084cc7411f61
Shaun Curtis <shaun@coldelm.com>
2024-12-17 11:05:56 +0000

    Add project files.

---
+++ SO79284001/Components/Server.cs
@@ -0,0 +1,9 @@
+﻿namespace SO79284001.Components;
+
+public class Server
+{
+    public int ServerId { get; set; }
+    public string City { get; set; } = "[Not Set]";
+    public string Name { get; set; } = "[Not Set]";
+    public bool IsOnline { get; set; }
+}
